437702bd089b930356586ffd8c5346b2ef58d0be
dhikdhiks <andhika.august4@smk.belajar.id>
2026-05-23 19:41:04 +0700

    with jupyter

--- projects/project.py
+++
@@ -1,8 +0,0 @@
-def hitung_kondisi_server(suhu: float, beban_cpu: int) -> str:
-    """Fungsi dengan Type Hinting untuk mendeteksi kesehatan server."""
-    if suhu > 75.0 or beban_cpu > 85:
-        return "WARNING: Server Overheat/Overload!"
-    return "STATUS: Server Aman."
-
-# Test Drive fungsi
-status_saat_ini = hitung_kondisi_server(72.5, 60)
